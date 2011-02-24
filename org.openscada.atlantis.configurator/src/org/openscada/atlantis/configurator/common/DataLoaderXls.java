a2304acf76aa6a2ef6c8a072ad4d5f52e9669838
Jens Reimann <jens.reimann@th4-systems.com>
2011-02-24 15:34:32 +0000

    read excel file using Apache PO
    git-svn-id: http://pubsvn.inavare.net/openscada/modules/deploy/trunk@8842 1b50e8ee-deed-0310-b1de-ab401e1c4d63

--- org.openscada.atlantis.configurator/src/org/openscada/atlantis/configurator/common/DataLoaderXls.java
+++ org.openscada.atlantis.configurator/src/org/openscada/atlantis/configurator/common/DataLoaderXls.java
@@ -1,65 +1,76 @@
 package org.openscada.atlantis.configurator.common;
 
 import java.io.File;
+import java.io.FileInputStream;
 import java.util.HashMap;
 import java.util.Map;
 
-import jxl.Sheet;
-import jxl.Workbook;
-import jxl.WorkbookSettings;
+import org.apache.poi.hssf.usermodel.HSSFFormulaEvaluator;
+import org.apache.poi.hssf.usermodel.HSSFSheet;
+import org.apache.poi.hssf.usermodel.HSSFWorkbook;
+import org.apache.poi.ss.usermodel.Cell;
+import org.apache.poi.ss.usermodel.Row;
 
 public class DataLoaderXls
 {
 
-    private final jxl.Workbook workbook;
+    private final HSSFWorkbook workbook;
 
     private final boolean useHeader;
 
     private final int skipHeaders;
 
+    private final int skipPastHeaders;
+
     public DataLoaderXls ( final File file ) throws Exception
     {
-        this ( file, true, 0, null );
+        this ( file, true, 0, 0, null );
     }
 
     public DataLoaderXls ( final File file, final boolean useHeader, final int skipHeaders, final String encoding ) throws Exception
     {
-        final WorkbookSettings s = new WorkbookSettings ();
-        if ( encoding != null )
-        {
-            s.setEncoding ( encoding );
-        }
-        this.workbook = Workbook.getWorkbook ( file, s );
+        this ( file, useHeader, skipHeaders, 0, encoding );
+    }
+
+    public DataLoaderXls ( final File file, final boolean useHeader, final int skipHeaders, final int skipPastHeaders, final String encoding ) throws Exception
+    {
+        final HSSFWorkbook wb = new HSSFWorkbook ( new FileInputStream ( file ) );
+
+        this.workbook = wb;
         this.useHeader = useHeader;
         this.skipHeaders = skipHeaders;
+        this.skipPastHeaders = skipPastHeaders;
     }
 
     public void load ( final int sheetIdx, final RowHandler rowHandler )
     {
-        final Sheet sheet = this.workbook.getSheet ( sheetIdx );
+        final HSSFSheet sheet = this.workbook.getSheetAt ( sheetIdx );
 
-        final Map<Integer, String> header;
+        Map<Integer, String> header = new HashMap<Integer, String> ();
 
-        for ( int i = 0; i < this.skipHeaders; i++ )
+        int i = 0;
+        for ( final Row row : sheet )
         {
-            loadRow ( sheet, i );
+            if ( i < this.skipHeaders )
+            {
+                System.out.println ( String.format ( "Skipping row %s", row.getRowNum () ) );
+                // skip
+            }
+            if ( i == this.skipHeaders )
+            {
+                if ( this.useHeader )
+                {
+                    System.out.println ( String.format ( "Loading row %s as header", row.getRowNum () ) );
+                    header = loadRow ( sheet, row );
+                }
+            }
+            if ( i > this.skipHeaders + this.skipPastHeaders )
+            {
+                rowHandler.handleRow ( i, mapRow ( header, loadRow ( sheet, row ) ) );
+            }
+            i++;
         }
 
-        if ( this.useHeader )
-        {
-            header = loadRow ( sheet, this.skipHeaders );
-        }
-        else
-        {
-            header = new HashMap<Integer, String> ();
-        }
-
-        final int rows = sheet.getRows ();
-
-        for ( int i = this.skipHeaders + 1; i < rows; i++ )
-        {
-            rowHandler.handleRow ( i, mapRow ( header, loadRow ( sheet, i ) ) );
-        }
     }
 
     private static Map<String, String> mapRow ( final Map<Integer, String> header, final Map<Integer, String> data )
@@ -82,30 +93,38 @@ public class DataLoaderXls
         return row;
     }
 
-    private static Map<Integer, String> loadRow ( final Sheet sheet, final int row )
+    private static String makeStringValue ( final HSSFWorkbook workbook, final Cell cell )
     {
-        final Map<Integer, String> header = new HashMap<Integer, String> ();
+        if ( cell == null )
+        {
+            return null;
+        }
+
+        final HSSFFormulaEvaluator eval = new HSSFFormulaEvaluator ( workbook );
+
+        switch ( cell.getCellType () )
+        {
+        case Cell.CELL_TYPE_BLANK:
+            return "";
+        case Cell.CELL_TYPE_FORMULA:
+            return eval.evaluate ( cell ).getStringValue ();
+        default:
+            return cell.toString ();
+        }
+    }
+
+    private static Map<Integer, String> loadRow ( final HSSFSheet sheet, final Row row )
+    {
+        final Map<Integer, String> rowData = new HashMap<Integer, String> ();
 
         int i = 0;
-        Object value;
-        while ( true )
+        for ( final Cell cell : row )
         {
-            try
-            {
-                value = sheet.getCell ( i, row ).getContents ();
-            }
-            catch ( final IndexOutOfBoundsException e )
-            {
-                break;
-            }
-
-            if ( value != null )
-            {
-                header.put ( i, value.toString () );
-            }
+            final String value = makeStringValue ( sheet.getWorkbook (), cell );
+            rowData.put ( i, value );
             i++;
         }
 
-        return header;
+        return rowData;
     }
 }
