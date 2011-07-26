fcddb27136484079610b393f24a8fc80ef2a6939
Jens Reimann <jens.reimann@th4-systems.com>
2011-07-26 18:13:47 +0200

    translate to de

--- org.openscada.atlantis.configurator/src/org/openscada/atlantis/configurator/summary/SumLoader.java
+++ org.openscada.atlantis.configurator/src/org/openscada/atlantis/configurator/summary/SumLoader.java
@@ -36,16 +36,16 @@ public class SumLoader
 
     public static void configureGroups ( final Configuration cfg, final Collection<SummaryGroup> groups, final Collection<Item> items, final int requiredSize )
     {
-        final Set<String> groupsSum = new HashSet<String> ( Arrays.asList ( "manual", "error", "alarm", "ackRequired", "blocked" ) );
+        final Set<String> groupsSum = new HashSet<String> ( Arrays.asList ( "manual", "error", "alarm", "ackRequired", "blocked" ) ); //$NON-NLS-1$ //$NON-NLS-2$ //$NON-NLS-3$ //$NON-NLS-4$ //$NON-NLS-5$
 
         for ( final SummaryGroup group : groups )
         {
             final String id = group.getId ();
             final Item item = ModelFactory.eINSTANCE.createItem ();
-            item.setDescription ( "Summary item for group: " + id );
-            item.setName ( id + ".sum" );
+            item.setDescription ( Messages.SumLoader_Description + id );
+            item.setName ( id + ".sum" ); //$NON-NLS-1$
             item.setAlias ( id );
-            item.setSystem ( "SCADA" );
+            item.setSystem ( Messages.SumLoader_System );
             item.setDataType ( DataType.INTEGER );
             item.setLocalManual ( false );
             item.setDefaultChain ( false );
@@ -55,7 +55,7 @@ public class SumLoader
             if ( group.getItems ().size () >= requiredSize )
             {
                 items.add ( item );
-                cfg.addSum ( id + ".sum", group.getItems (), groupsSum );
+                cfg.addSum ( id + ".sum", group.getItems (), groupsSum ); //$NON-NLS-1$
             }
         }
     }
