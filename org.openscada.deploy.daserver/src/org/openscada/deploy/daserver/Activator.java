b7a1d4fa0c223df7733fa1cc1e5c1f34da516653
Jens Reimann <jens.reimann@th4-systems.com>
2012-03-13 17:24:19 +0100

    trying to add "daserver"

---
+++ org.openscada.deploy.daserver/src/org/openscada/deploy/daserver/Activator.java
@@ -0,0 +1,19 @@
+package org.openscada.deploy.daserver;
+
+import org.osgi.framework.BundleActivator;
+import org.osgi.framework.BundleContext;
+
+public class Activator implements BundleActivator
+{
+
+    @Override
+    public void start ( final BundleContext context ) throws Exception
+    {
+    }
+
+    @Override
+    public void stop ( final BundleContext context ) throws Exception
+    {
+    }
+
+}
---
+++ org.openscada.deploy.daserver/src/org/openscada/deploy/daserver/Application.java
@@ -0,0 +1,20 @@
+package org.openscada.deploy.daserver;
+
+import org.eclipse.equinox.app.IApplication;
+import org.eclipse.equinox.app.IApplicationContext;
+
+public class Application implements IApplication
+{
+
+    @Override
+    public Object start ( final IApplicationContext context ) throws Exception
+    {
+        return null;
+    }
+
+    @Override
+    public void stop ()
+    {
+    }
+
+}
