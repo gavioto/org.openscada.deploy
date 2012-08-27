ac0a3a4a39096ab20183c6d88935539fba05b8d7
Jens Reimann <jens.reimann@th4-systems.com>
2012-08-27 10:38:20 +0200

    remove "old" configuration path

--- org.openscada.atlantis.configurator/src/org/openscada/atlantis/configurator/Application.java
+++ org.openscada.atlantis.configurator/src/org/openscada/atlantis/configurator/Application.java
@@ -14,8 +14,6 @@ import javax.script.ScriptException;
 import org.eclipse.core.runtime.FileLocator;
 import org.eclipse.equinox.app.IApplication;
 import org.eclipse.equinox.app.IApplicationContext;
-import org.openscada.atlantis.configurator.formula.FormulaModule;
-import org.openscada.atlantis.configurator.script.ScriptModule;
 import org.openscada.atlantis.configurator.summary.SumLoader;
 import org.openscada.atlantis.configurator.summary.SummaryGenerator;
 import org.openscada.configuration.model.Module;
@@ -60,82 +58,6 @@ public class Application implements IApplication
     @Override
     public void stop ()
     {
-    }
-
-    private static void processOld ( final PrintStream log, final LinkedList<String> arguments ) throws Exception
-    {
-        final File scriptDBase = new File ( arguments.pop () );
-        final File base = new File ( arguments.pop () );
-        final File scriptBase = new File ( base, "script/js" );
-        final File generatedBase = new File ( base, "generated" );
-
-        final Configuration cfg = new Configuration ( base );
-
-        final String outFile = arguments.pop ();
-        final String basicsFile = arguments.pop ();
-
-        log.println ( "** 0 - Loading scripts" );
-        log.println ( "*** 0a - Script Loader" );
-        org.openscada.atlantis.configurator.script.ScriptLoader.loadScripts ( new File ( base, "input/scripts.ods" ), new File ( base, "input/scripts" ) );
-        log.println ( "*** 0b - Formulas Loader" );
-        arguments.push ( LegacyForumalsLoader.loadScript ( cfg, new File ( base, "input/PARSERformulas" ), scriptBase, new File ( generatedBase, "IOList-generated-formulas-script.xls" ) ) );
-        log.println ( "*** 0c - Summary groups" );
-        arguments.push ( SumLoader.convertGroups ( cfg, new File ( base, "input/summary.ods" ), new File ( generatedBase, "IOList-generated-sum.xls" ), SUMMARY_REQUIRED_SIZE ) );
-
-        arguments.push ( processNetwork ( log, System.getProperty ( "prefix", "XXX" ), base, generatedBase ) );
-
-        final Collection<String> overrides = new LinkedList<String> ();
-
-        log.println ( "** 1 - Loading data" );
-        log.println ( "*** 1a - Loading basic configuration" );
-        loadBasics ( log, cfg, new File ( basicsFile ) );
-
-        log.println ( "*** 1b - Loading data files" );
-        for ( final String file : arguments )
-        {
-            if ( !file.contains ( "override" ) )
-            {
-                log.println ( " ** Loading: " + file );
-                final List<Item> loadedItems = SpreadSheetPoiHelper.loadExcel ( file );
-                cfg.addItems ( loadedItems );
-                log.println ( String.format ( " ** Loaded %s items from %s", loadedItems.size (), file ) );
-            }
-            else
-            {
-                overrides.add ( file );
-            }
-        }
-
-        log.println ( "*** 1c - Process Formulas" );
-        FormulaModule.process ( new File ( base, "input/formulas" ), cfg );
-
-        log.println ( "*** 1d - Process Scripts" );
-        ScriptModule.process ( new File ( base, "input/scripts" ), cfg );
-
-        log.println ( "*** 1e - Apply overrides" );
-        for ( final String file : overrides )
-        {
-            cfg.applyOverrides ( SpreadSheetPoiHelper.loadExcel ( file ) );
-        }
-
-        log.println ( "*** 1f - Apply script overrides" );
-        applyScriptOverrides ( scriptDBase, cfg );
-
-        log.println ( "** 3 - Process" );
-        cfg.removeInactive ();
-        SummaryGenerator.generateSummaryAlarms ( cfg, cfg.getItems (), SUMMARY_REQUIRED_SIZE );
-        cfg.generateItems ();
-        cfg.generateGlobalSummaries ();
-
-        log.println ( "** 4a - Validating" );
-        cfg.validate ();
-
-        log.println ( "** 5 - Output" );
-        log.println ( " **  Base Dir: " + outFile );
-        cfg.write ( new File ( outFile ) );
-
-        log.println ( "** 5 - Output finished" );
-        cfg.close ();
     }
 
     private static void processNew ( final PrintStream log, final Project project ) throws Exception
@@ -254,7 +176,7 @@ public class Application implements IApplication
         }
         else
         {
-            processOld ( log, arguments );
+            // error
         }
 
     }
@@ -278,28 +200,6 @@ public class Application implements IApplication
 
         log.println ( "**** 1ae - Loading basic configuration - AE Filter" );
         loader.load ( 4, new EventFilterHandler ( cfg ) );
-    }
-
-    /**
-     * @deprecated use the new module system instead
-     * @author Jens Reimann
-     */
-    @Deprecated
-    private static String processNetwork ( final PrintStream log, final String prefix, final File base, final File generatedBase ) throws Exception
-    {
-        log.println ( "*** 0d - Processing network" );
-        final File outListFile = new File ( generatedBase, "IOList-generated-exec.xls" );
-        final File sourceFile = new File ( base, "input/network.ods" );
-        final File xmlOutFile = new File ( generatedBase, "network.xml" );
-
-        if ( !sourceFile.exists () )
-        {
-            return null;
-        }
-
-        org.openscada.atlantis.configurator.exec.Application.process ( prefix, outListFile.getAbsolutePath (), sourceFile.getAbsolutePath (), xmlOutFile.getAbsolutePath () );
-
-        return outListFile.getAbsolutePath ();
     }
 
     private static void applyScriptOverrides ( final File base, final Configuration cfg ) throws FileNotFoundException, ScriptException
