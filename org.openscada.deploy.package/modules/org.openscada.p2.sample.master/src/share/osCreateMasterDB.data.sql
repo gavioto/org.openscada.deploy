e736cd926f7a11d34ffda26628cefd969c938da2
Jürgen Rose <juergen.rose@th4-systems.com>
2012-11-30 11:48:52 +0100

    make passwords explicit

--- org.openscada.deploy.package/modules/org.openscada.p2.sample.master/src/share/osCreateMasterDB.data.sql
+++ org.openscada.deploy.package/modules/org.openscada.p2.sample.master/src/share/osCreateMasterDB.data.sql
@@ -5,7 +5,8 @@ insert into os_groups ( group_id ) values ( 'admin' );
 
 -- user: admin/admin
 
-insert into os_users ( user_id, password, enabled ) values ( 'admin', '21232f297a57a5a743894a0e4a801fc3', true );
+insert into os_users ( user_id, password, enabled ) values ( 'admin', md5('admin'), true );
+insert into os_users ( user_id, password, enabled ) values ( 'openscada', md5('openscada'), true );
 
 insert into os_group_assigns ( user_id, group_id ) values ( 'openscada', 'admin' );
 insert into os_group_assigns ( user_id, group_id ) values ( 'admin', 'admin' );
