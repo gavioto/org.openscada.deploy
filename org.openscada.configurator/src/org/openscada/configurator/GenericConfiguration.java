9e57464247fb848327107f2824e4924e6d0e3c96
Jürgen Rose <cptmauli@googlemail.com>
2013-04-30 14:31:28 +0200

    also create file ca as zip file

--- org.openscada.configurator/src/org/openscada/configurator/GenericConfiguration.java
+++ org.openscada.configurator/src/org/openscada/configurator/GenericConfiguration.java
@@ -1,3 +1,25 @@
+/*
+ * This file is part of the openSCADA project
+ * 
+ * Copyright (C) 2006-2012 TH4 SYSTEMS GmbH (http://th4-systems.com)
+ * Copyright (C) 2013 Jens Reimann (ctron@dentrassi.de)
+ * Copyright (C) 2013 Jürgen Rose (cptmauli@googlemail.com)
+ *
+ * openSCADA is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU Lesser General Public License version 3
+ * only, as published by the Free Software Foundation.
+ *
+ * openSCADA is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU Lesser General Public License version 3 for more details
+ * (a copy is included in the LICENSE file that accompanied this code).
+ *
+ * You should have received a copy of the GNU Lesser General Public License
+ * version 3 along with openSCADA. If not, see
+ * <http://opensource.org/licenses/lgpl-3.0.html> for a copy of the LGPLv3 License.
+ */
+
 package org.openscada.configurator;
 
 import java.io.File;
@@ -8,10 +30,12 @@ import java.io.FileWriter;
 import java.io.IOException;
 import java.io.PrintStream;
 import java.net.URL;
+import java.net.URLEncoder;
 import java.util.Arrays;
 import java.util.HashMap;
 import java.util.HashSet;
 import java.util.Map;
+import java.util.Properties;
 import java.util.Set;
 import java.util.zip.ZipEntry;
 import java.util.zip.ZipOutputStream;
@@ -118,6 +142,8 @@ public class GenericConfiguration implements ConfigurationTarget
 
         makeOscar ( dataFile, ignoreFieldsFile, new File ( baseDir, "configuration.oscar" ) );
 
+        makeCa ( new File ( baseDir, "ca.zip" ) );
+
         writeXml ( new File ( baseDir, "objects.xml" ) );
     }
 
@@ -200,6 +226,32 @@ public class GenericConfiguration implements ConfigurationTarget
             storeFile ( ignoreFieldsFile, zout, "ignoreFields.json" );
         }
 
+        zout.close ();
+    }
+
+    private void makeCa ( final File caFile ) throws IOException
+    {
+        final ZipOutputStream zout = new ZipOutputStream ( new FileOutputStream ( caFile ) );
+        for ( final Map.Entry<String, Map<String, Map<String, String>>> factory : this.data.entrySet () )
+        {
+            final String factoryId = factory.getKey ();
+            zout.putNextEntry ( new ZipEntry ( factoryId + File.separator ) );
+            zout.putNextEntry ( new ZipEntry ( factoryId + File.separator + ".meta" ) );
+            Properties p = new Properties ();
+            p.put ( "id", factoryId );
+            p.store ( zout, "" );
+            for ( final Map.Entry<String, Map<String, String>> conf : factory.getValue ().entrySet () )
+            {
+                final String confId = URLEncoder.encode ( conf.getKey (), "UTF-8" );
+                zout.putNextEntry ( new ZipEntry ( factoryId + File.separator + confId ) );
+                p = new Properties ();
+                for ( final Map.Entry<String, String> entry : conf.getValue ().entrySet () )
+                {
+                    p.put ( entry.getKey (), entry.getValue () );
+                }
+                p.store ( zout, "" );
+            }
+        }
         zout.close ();
     }
 
