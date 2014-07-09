452fd9222458de22053372194e999ebc1a66823a
Juergen Rose <juergen.rose@ibh-systems.com>
2014-07-09 16:29:36 +0200

    removed old configurator

--- org.openscada.configurator/src/org/openscada/configurator/data/ConfigurationTarget.java
+++
@@ -1,8 +0,0 @@
-package org.openscada.configurator.data;
-
-import java.util.Map;
-
-public interface ConfigurationTarget
-{
-    public void addData ( String factoryId, String id, Map<String, String> data );
-}
